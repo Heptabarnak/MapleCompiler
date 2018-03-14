62e54904fb77385e99f7e21c05b07bcd81568acf
Justin GALLY <justin.gallysp@gmail.com>
2018-03-14 10:34:41 +0100

    Start of SymboleTable Package

---
+++ front-end/symbole-table/Symbole.h
@@ -0,0 +1,16 @@
+//
+// Created by jgally on 14/03/18.
+//
+#include <SymboleTable.h>
+#ifndef ANTLR4CPP_FETCHER_SYMBOLE_H
+#define ANTLR4CPP_FETCHER_SYMBOLE_H
+
+class SymboleTable{};
+
+class Symbole {
+public:
+    SymboleTable table;
+};
+
+
+#endif //ANTLR4CPP_FETCHER_SYMBOLE_H
---
+++ front-end/symbole-table/SymboleTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jgally on 14/03/18.
+//
+
+#include "SymboleTable.h"

b489cea13902dcde7bd64d59c81b60d40c5e0bcf
tbourret <tombourret@free.fr>
2018-03-14 10:34:05 +0100

    BlockFunction class added

---
+++ front-end/function/BlockFunction.h
@@ -0,0 +1,14 @@
+//
+// Created by tbourret on 14/03/18.
+//
+
+#ifndef ANTLR4CPP_FETCHER_BLOCKFUNCTION_H
+#define ANTLR4CPP_FETCHER_BLOCKFUNCTION_H
+
+
+class BlockFunction {
+
+};
+
+
+#endif //ANTLR4CPP_FETCHER_BLOCKFUNCTION_H

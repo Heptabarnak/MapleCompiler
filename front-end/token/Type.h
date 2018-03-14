37d2f0ee999b71489520fd8956fab4b9890b1e70
Justin GALLY <justin.gallysp@gmail.com>
2018-03-14 09:45:00 +0100

    Package : Token + lib

---
+++ front-end/token/Type.h
@@ -0,0 +1,23 @@
+//
+// Created by jgally on 14/03/18.
+//
+
+#ifndef ANTLR4CPP_FETCHER_TYPE_H
+#define ANTLR4CPP_FETCHER_TYPE_H
+
+
+enum Type {
+    CHAR,
+    INT32_T,
+    INT64_T
+};
+
+enum FunctionType {
+    CHAR,
+    INT32_T,
+    INT64_T,
+    VOID
+};
+
+
+#endif //ANTLR4CPP_FETCHER_TYPE_H

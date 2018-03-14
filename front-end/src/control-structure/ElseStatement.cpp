1bcde9ef0d9a9f7768bc35775a106b4813bef525
Julien Charles-Nicolas <julien.charles-nicolas@insa-lyon.fr>
2018-03-14 11:36:58 +0100

    ControleStructure not finished

---
+++ front-end/src/control-structure/ElseStatement.cpp
@@ -0,0 +1,7 @@
+//
+// Created by julien on 3/14/18.
+//
+
+#include "ElseStatement.h"
+
+ElseStatement::ElseStatement(Instruction *inst) : instruction(inst){}
---
+++ front-end/src/control-structure/IfStatementInstruction.cpp
@@ -0,0 +1,5 @@
+//
+// Created by julien on 3/14/18.
+//
+
+#include "IfStatementInstruction.h"

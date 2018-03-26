c70bd4720046af37fce6d2254db89f2e8f943ef4
Tom B <tom.bourret@insa-lyon.fr>
2018-03-26 08:42:29 +0200

    TODO : putchar and getchar added to SymbolTable

--- front-end/src/visitor/StartVisitor.cpp
+++ front-end/src/visitor/StartVisitor.cpp
@@ -9,8 +9,17 @@ using std::vector;
 
 antlrcpp::Any StartVisitor::visitStart(MapleGrammarParser::StartContext *ctx) {
     currentSymbolTable = new SymbolTable();
+    vector<FunctionParam*> vectorParamsPutchar;
+    vectorParamsPutchar.push_back(new FunctionParam("char", INT64_T));
+    vector<FunctionParam*> vectorParamsGetchar;
+    FunctionDefinition* putchar = new FunctionDefinition(nullptr, INT64_T, vectorParamsPutchar, "putchar");
+    FunctionDefinition* getchar = new FunctionDefinition(nullptr, INT64_T, vectorParamsGetchar, "getchar");
 
-    // TODO Add putchar & getchar
+    auto symbolPutchar = new Symbol(currentSymbolTable, putchar, true, true);
+    auto symbolGetchar = new Symbol(currentSymbolTable, getchar, true, true);
+
+    currentSymbolTable->insert("putchar", symbolPutchar);
+    currentSymbolTable->insert("getchar", symbolGetchar);
 
     return start = new Start(
             *mapContext2VectorFlat<MapleGrammarParser::ProgramContext *, Declaration *>(ctx->program(), this),

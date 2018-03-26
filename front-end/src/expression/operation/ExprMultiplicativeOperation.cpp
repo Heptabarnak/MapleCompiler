d9053e32f9cc71192e7f82819029b74a570ecdb0
Tom B <tom.bourret@insa-lyon.fr>
2018-03-26 08:20:02 +0200

    FIX : division by 0 is handled (return an error)

--- front-end/src/expression/operation/ExprMultiplicativeOperation.cpp
+++ front-end/src/expression/operation/ExprMultiplicativeOperation.cpp
@@ -1,5 +1,9 @@
 #include <str2int.h>
+#include <iostream>
 #include "ExprMultiplicativeOperation.h"
+
+using std::cerr;
+using std::endl;
 
 ExprMultiplicativeOperation::ExprMultiplicativeOperation(Expr *left, Expr *right, const string &op)
         : ExprOperation(left, right) {
@@ -25,8 +29,13 @@ long ExprMultiplicativeOperation::simplify() {
         case MULT:
             return leftExpr->simplify() * rightExpr->simplify();
         case DIV:
-            // FIXME Check 0 division
-            return leftExpr->simplify() / rightExpr->simplify();
+            if (rightExpr->simplify() != 0) {
+                return leftExpr->simplify() / rightExpr->simplify();
+            }
+            else {
+                cerr << "Division by 0 is forbidden" << endl;
+                throw std::runtime_error("Forbidden operation : division by zero");
+            }
         case MOD:
             return leftExpr->simplify() % rightExpr->simplify();
     }

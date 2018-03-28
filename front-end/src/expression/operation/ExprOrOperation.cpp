975cc417807240f0fa35d525d91ea52f2f4969b8
Marc-Antoine Fernandes <embraser01@gmail.com>
2018-03-28 19:48:23 +0200

    Add OR operation (little bit tricky)

--- front-end/src/expression/operation/ExprOrOperation.cpp
+++ front-end/src/expression/operation/ExprOrOperation.cpp
@@ -1,5 +1,4 @@
-
-
+#include <ir/instructions/LoadConstInstr.h>
 #include "ExprOrOperation.h"
 
 ExprOrOperation::ExprOrOperation(Expr *left, Expr *right) : ExprOperation(left, right) {
@@ -12,6 +11,48 @@ long ExprOrOperation::simplify() {
 
 string ExprOrOperation::buildIR(CFG *cfg) {
 
-    // TODO If statement
-    return std::__cxx11::string();
+    // We want to do something like this:
+    //  if (left) {var = 1;}
+    //  else {if (right) {var = 1;}
+
+    string finalValue = cfg->createNewTmpVar(Type::INT64_T);
+
+    // Two options for the value of an or expression: 0 or 1
+    auto falseBB = new BasicBlock(cfg, cfg->newBBName());
+    auto trueBB = new BasicBlock(cfg, cfg->newBBName());
+    auto nextBB = new BasicBlock(cfg, cfg->newBBName());
+    auto rightTestBB = new BasicBlock(cfg, cfg->newBBName());
+
+    // Set return value to 1 or 0
+    falseBB->addIRInstr(new LoadConstInstr(falseBB, finalValue, 0));
+    trueBB->addIRInstr(new LoadConstInstr(trueBB, finalValue, 1));
+
+
+    // Left test
+    leftExpr->buildIR(cfg);
+
+    cfg->currentBB->exitTrue = trueBB; // If first test is ok, directly jump to 'finalResult = 1'
+    cfg->currentBB->exitFalse = rightTestBB; // If not, try with the right expr
+
+    cfg->addBB(rightTestBB);
+    cfg->addBB(trueBB);
+
+    cfg->currentBB = rightTestBB; // Set rightBB as current BB before 'rightExpr#buildIR()'
+
+    // Right test
+    rightExpr->buildIR(cfg);
+
+    cfg->currentBB->exitTrue = trueBB; // If right test is ok, jump to 'finalResult = 1'
+    cfg->currentBB->exitFalse = falseBB; // If not, jump to 'finalResult = 0'
+
+    cfg->addBB(falseBB);
+
+    falseBB->exitTrue = nextBB; // Anyway, after all this, jump to a new BasicBlock
+    trueBB->exitTrue = nextBB;
+
+    cfg->addBB(falseBB);
+    cfg->addBB(nextBB);
+    cfg->currentBB = nextBB; // Set the currentBB to a new BB
+
+    return finalValue;
 }

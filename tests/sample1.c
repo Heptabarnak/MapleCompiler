60e5a932b263d26a479fd8b72fdcf6026c4aa525
Marc-Antoine Fernandes <embraser01@gmail.com>
2018-03-29 22:50:23 +0200

    "Hello World!" Fix #16

--- tests/sample1.c
+++ tests/sample1.c
@@ -4,5 +4,17 @@ void main() {
     int64_t x = 1;
     int64_t y = 2;
     int64_t z = 3;
-    putchar('a');
+    putchar('H');
+    putchar('e');
+    putchar('l');
+    putchar('l');
+    putchar('o');
+    putchar(' ');
+    putchar('W');
+    putchar('o');
+    putchar('r');
+    putchar('l');
+    putchar('d');
+    putchar('!');
+    putchar('\n');
 }

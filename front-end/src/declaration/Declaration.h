ed591e5edcc7536bf37abea6e45c7406c8a57009
loicsrz <ljedi007@gmail.com>
2018-03-29 13:33:47 +0200

    Add BuildIR for Declaration and some modification in VarDeclaration

--- front-end/src/declaration/Declaration.h
+++ front-end/src/declaration/Declaration.h
@@ -1,10 +1,14 @@
 #ifndef MAPLECOMPILER_DECLARATION_H
 #define MAPLECOMPILER_DECLARATION_H
 
+
+#include <ir/BuildIR.h>
+
 class Declaration {
 
 public:
     virtual ~Declaration() = default;
+
 };
 
 
--- front-end/src/variable/VarDeclaration.cpp
+++ front-end/src/variable/VarDeclaration.cpp
@@ -1,3 +1,4 @@
+#include <ir/instructions/WMemInstr.h>
 #include "VarDeclaration.h"
 
 VarDeclaration::VarDeclaration(const string &name, Type type, Expr *assignment) : name(name), type(type),
@@ -8,5 +9,10 @@ const string &VarDeclaration::getName() const {
 }
 
 string VarDeclaration::buildIR(CFG *cfg) {
-    return name;
+    std::string value = cfg->createNewTmpVar(Type::INT64_T);
+    std::string varAssignment = assignment->buildIR(cfg);
+
+    cfg->addIRInstr(new WMemInstr(cfg->currentBB, value, varAssignment));
+
+    return value;
 }

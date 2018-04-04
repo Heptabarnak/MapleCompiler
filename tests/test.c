38b48d9c302131aeb2a9b493fe2f26f23f3d7b20
Justin GALLY <justin.gallysp@gmail.com>
2018-04-04 09:41:41 +0200

    Adding of prototype  please never modify it

---
+++ tests/test.c
@@ -0,0 +1,15 @@
+//DO NOT USE
+#include<inttypes.h>
+
+int64_t globA = 5;
+int64_t globB;
+int64_t globC[5] = {1, 2, 3};
+int64_t globD[10];
+
+
+void main() {
+    int64_t x = 2;
+
+    putchar('0' + globC[1]);
+    putchar('\n');
+}
